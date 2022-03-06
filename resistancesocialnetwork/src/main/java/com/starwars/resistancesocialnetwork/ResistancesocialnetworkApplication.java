175be204d560cd9cf77eaba50c18bbb4cd283fd4
GuiTodys <guilhermetodys123@hotmail.com>
2022-03-06 17:42:38 -0300

    Project start

---
+++ resistancesocialnetwork/src/main/java/com/starwars/resistancesocialnetwork/ResistancesocialnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.starwars.resistancesocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResistancesocialnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResistancesocialnetworkApplication.class, args);
+	}
+
+}
---
+++ resistancesocialnetwork/src/test/java/com/starwars/resistancesocialnetwork/ResistancesocialnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starwars.resistancesocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResistancesocialnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
