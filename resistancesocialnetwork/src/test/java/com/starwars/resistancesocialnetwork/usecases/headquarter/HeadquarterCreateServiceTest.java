eaf0f54991fe335cf28fb13483306207ad363b8b
DouglasHLima <douglashdelima@gmail.com>
2022-03-07 21:42:22 -0300

    feat: build test for creation method

---
+++ resistancesocialnetwork/src/test/java/com/starwars/resistancesocialnetwork/usecases/headquarter/HeadquarterCreateServiceTest.java
@@ -0,0 +1,35 @@
+package com.starwars.resistancesocialnetwork.usecases.headquarter;
+
+import com.starwars.resistancesocialnetwork.builders.domain.HeadQuarterDomainBuilder;
+import com.starwars.resistancesocialnetwork.domains.Headquarter;
+import com.starwars.resistancesocialnetwork.gateways.persistance.HeadQuartersPersistenceGateway;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.hamcrest.MockitoHamcrest;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class HeadquarterCreateServiceTest {
+
+    @Mock
+    HeadQuartersPersistenceGateway headQuartersPersistenceGateway;
+    @InjectMocks HeadquarterCreateService createService;
+
+    @Test
+    void whenInformedAValidHeadQuarterThenItsShouldBeCreated() {
+        //given
+        Headquarter expectedSaveHeadQuarter = HeadQuarterDomainBuilder.builder().build().toDomain();
+
+        //when
+        Mockito.when(headQuartersPersistenceGateway.save(expectedSaveHeadQuarter)).thenReturn(expectedSaveHeadQuarter);
+
+        //then
+        Headquarter createdHeadQuarter = createService.execute(expectedSaveHeadQuarter);
+
+        Assertions.assertThat(createdHeadQuarter).isEqualTo(expectedSaveHeadQuarter);
+    }
+}
