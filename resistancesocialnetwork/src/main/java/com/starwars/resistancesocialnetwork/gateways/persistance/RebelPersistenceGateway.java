67011ab5495101d1a578bad62c63732e5af89ae7
GuiTodys <guilhermetodys123@hotmail.com>
2022-03-09 17:48:01 -0300

    feat: Added Swagger configuration

--- resistancesocialnetwork/src/main/java/com/starwars/resistancesocialnetwork/gateways/persistance/RebelPersistenceGateway.java
+++ resistancesocialnetwork/src/main/java/com/starwars/resistancesocialnetwork/gateways/persistance/RebelPersistenceGateway.java
@@ -1,5 +1,6 @@
 package com.starwars.resistancesocialnetwork.gateways.persistance;
 
+import com.starwars.resistancesocialnetwork.domains.Headquarter;
 import com.starwars.resistancesocialnetwork.domains.Rebel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,7 +8,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 
 public interface RebelPersistenceGateway {
-    Rebel save(Rebel rebel);
+    Rebel save(Rebel rebel, Headquarter headquarter);
     Optional<Rebel> findById(Long id);
     boolean existById(Long id);
     Page<Rebel> findAll(Pageable page);
