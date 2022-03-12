e2f5518043585fbf0ae47c93c89ab4696577c79d
GuiTodys <guilhermetodys123@hotmail.com>
2022-03-12 20:11:21 -0300

    feat: added a report option to find rebels percentage
    * Created Domain "Percentages".
    * Created "Percentage Response Mapper" interface  under "...\gateways\controllers\mappers\response".
    * Created "ReportController" under ."..\gateways\controllers".
    * Created "Percentage Response" under "...\gateways\controllers\response"
    * Created "ReportPercentage" service under "...\usecases\reports"

---
+++ resistancesocialnetwork/src/main/java/com/starwars/resistancesocialnetwork/usecases/reports/ReportPercentage.java
@@ -0,0 +1,30 @@
+package com.starwars.resistancesocialnetwork.usecases.reports;
+
+import com.starwars.resistancesocialnetwork.domains.Percentages;
+import com.starwars.resistancesocialnetwork.domains.Rebel;
+import com.starwars.resistancesocialnetwork.usecases.rebel.RebelGetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ReportPercentage {
+
+    private final RebelGetService rebelGetService;
+
+    public Percentages execute(Pageable page){
+        List<Rebel> rebels = rebelGetService.getAll(page).getContent();
+        List<Rebel> traitors = rebels.stream().filter(rebel -> rebel.getTraitor()).collect(Collectors.toList());
+        Float traitorsPercentage = Integer.valueOf(traitors.size()).floatValue() / Integer.valueOf(rebels.size()).floatValue() * 100;
+
+        List<Rebel> notTraitors = rebels.stream().filter(rebel -> !rebel.getTraitor()).collect(Collectors.toList());
+        Float notTraitorsPercentage = Integer.valueOf(notTraitors.size()).floatValue() / Integer.valueOf(rebels.size()).floatValue() * 100;
+
+
+        return Percentages.builder().notTraitorsPercentage(notTraitorsPercentage).traitorsPercentage(traitorsPercentage).build();
+    }
+}
